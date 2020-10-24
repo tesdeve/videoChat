a0502db65f0fa3716186233c10a5cea7b6d6f22b
tesdeve <tesdeve@gmail.com>
2020-10-24 14:46:29 +0100

    adding root to app

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # sends a post request to the show method in the sessions controller with a parameter passed from the view
     post '/sessions', to: 'sessions#show', as: :sessions_show
   
-  get 'welcome/index'
+  root 'welcome/index'
   resources :subjects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
